c442e057e0e67de908b1972560b16f6f25acb051
Ilnur Galiullin <shidan.dc@gmail.com>
2017-04-17 13:25:34 +0300

    Базовый вывод json

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,22 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    fdata = open(filepath, 'r')
+    return fdata.read().strip()
 
 
 def pretty_print_json(data):
-    pass
+    json_data = json.loads(data)
+    print(json.dumps(json_data, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1:
+        print("Укажите имя файла для вывода")
+    elif len(sys.argv) == 2:
+        pretty_print_json(load_data(sys.argv[1]))
+    else:
+        print("Слишком много параметров")
+    
